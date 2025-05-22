08e2523a71e010912d209b004c230b5205d4e84c
aayushk18 <kumaraayush1807@gmail.com>
2025-05-22 21:29:22 +0530

    Update Header.jsx

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -63,76 +63,7 @@ const Header = ({ users }) => {
 
 
     return (
-        // <>
-        //     <div className=' fixed bg-white flex flex-row w-full p-2 justify-between items-center text-2xl shadow-xs shadow-gray-500 h-16 z-50'>
-        //         <div className='font-bold'>
-        //             <NavLink className='text-4xl ' to='/'><i class="uil uil-apple"> </i></NavLink>
-        //             <Link to='/'>iTalk</Link>
-        //         </div>
-
-
-        //         <form className='gap-5 flex flex-row'>
-        //             <input required type="search" name="" id="" placeholder='Search Here' className=' bg-gray-300 w-100 pl-5 p-2 rounded-full text-lg' />
-        //             <button type="submit" className=' bg-gray-300  aspect-square p-2 rounded-full text-gray-500 items-center text-center '><i class="uil uil-search-alt"></i></button>
-
-        //         </form>
-
-
-        //         <div className='flex flex-row gap-5 align-middle p-1'>
-
-
-        //             <div className='group cursor-pointer text-3xl' id='request' >
-        //                 <div>
-        //                     <i class="uil uil-users-alt"></i>
-        //                 </div>
-        //                 <div className='absolute hidden group-hover:block right-2' >
-        //                     <RequestBox users={users} />
-        //                 </div>
-        //             </div>
-
-
-        //             <div className=' group cursor-pointer text-3xl'>
-        //                 <div>
-
-        //                     <i class="uil uil-comment-alt"></i>
-        //                 </div>
-        //                 <div className='absolute hidden group-hover:block right-2'>
-        //                     <MessageBox users={users} />
-        //                 </div>
-        //             </div>
-
-
-        //             <div className=' group cursor-pointer text-3xl '>
-        //                 <div>
-        //                     <i class="uil uil-bell"></i>
-        //                 </div>
-        //                 <div className='absolute hidden group-hover:block right-2'>
-        //                     <NotificationBox users={users} />
-        //                 </div>
-        //             </div>
-        //             <div>
-        //                 <NavLink className={({ isActive }) => isActive ? 'border-blue-500' : ''} to='profile' ><img src="https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/tony_stark.png" className='absolute bg-fuchsia-300 border-2 border-white shadow-sm  rounded-full h-10 w-10 ' alt="Example" /></NavLink> ...
-        //             </div>
-
-
-
-
-        //             <div className=' group cursor-pointer text-3xl' >
-        //                 <div>
-        //                     <i class="uil uil-angle-down"></i>
-        //                 </div>
-        //                 <div className='absolute hidden group-hover:block  right-2   '>
-        //                     <ul className='bg-white shadow-xl shadow-gray-500 translate-y-7 w-50 rounded-lg text-lg  p-3 right-2'>
-        //                         <li className='cursor-pointer' >Report a Bug</li>
-        //                         <li className='cursor-pointer' >Settings</li>
-        //                         <li className='cursor-pointer' >Log out</li>
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //         </div>
-
-        //     </div >
-        // </>
+      
 
 
         <>
@@ -232,7 +163,7 @@ const Header = ({ users }) => {
 
                 {/* Mobile Menu (Hidden on md+) */}
                 {showMenu && (
-                    <div className="flex flex-col bg-white rounded-md shadow-gray-600 shadow-sm md:hidden gap-5 mt-3 px-4">
+                    <div className="flex flex-col bg-white rounded-md shadow-gray-600 shadow-sm md:hidden gap-4 mt-3 px-4">
                         <form className="flex m-2 gap-2 items-center">
                             <input
                                 required
@@ -248,11 +179,7 @@ const Header = ({ users }) => {
                             </button>
                         </form>
 
-                        <div className='flex flex-row  text-gray-700 my-5 gap-3 flex-wrap'>
-                            <img className='rounded-full h-10 w-10  border-2 border-white shadow-gray-900 shadow-sm' src="https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/tony_stark.png" alt="" />
-                            <Link to='/profile' onClick={() => setShowMenu(!showMenu)} className="  text-4xl cursor-pointer">Aayush Kumar</Link>
-
-                        </div>
+                       
 
 
 
@@ -260,6 +187,12 @@ const Header = ({ users }) => {
                             <i onClick={() => setShowReq(!showReq)} className={showReq ? "uil uil-users-alt text-green-500" : "uil uil-users-alt text-gray-500"}></i>
                             <i onClick={() => setShowMesg(!showMesg)} className={showMesg ? "uil uil-comment-alt text-green-500 " : "uil uil-comment-alt text-gray-500"}></i>
                             <i onClick={() => setShowNotification(!showNotification)} className={showNotification ? "uil uil-bell text-green-500" : "uil uil-bell text-gray-500"}></i>
+                        </div>
+
+                         <div className='flex flex-row  text-gray-700 gap-3 flex-wrap'>
+                            <img className='rounded-full h-10 w-10  border-2 border-white shadow-gray-900 shadow-sm' src="https://mediaproxy.tvtropes.org/width/1200/https://static.tvtropes.org/pmwiki/pub/images/tony_stark.png" alt="" />
+                            <Link to='/profile' onClick={() => setShowMenu(!showMenu)} className="  text-4xl cursor-pointer">Aayush Kumar</Link>
+
                         </div>
 
                         <div>
